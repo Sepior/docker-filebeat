faea22701cec22e86080100f07c5ac2f442c2cff
Thomas P. Jakobsen <tpj@sepior.com>
2017-08-24 09:41:19 +0200

    Add deploy file for sepior image

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME="docker.sepior.net:5000/sepior/docker-filebeat"
+source ./local/creds.sh
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD "https://docker.sepior.net:5000"
+
+docker build --tag $IMAGE_NAME 5-stdin 
+
+docker push $IMAGE_NAME
